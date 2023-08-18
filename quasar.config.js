c203a30230b3e28b40a7e989003ebcffa4fdcc7f
somnuk2 <wongsaphatbbgun@gmail.com>
2023-08-18 16:50:45 +0700

    Add 'Notify'in quasar.comf.js

--- quasar.config.js
+++ quasar.config.js
@@ -98,7 +98,8 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [notify]
+      
     },
 
     // animations: 'all', // --- includes all animations
